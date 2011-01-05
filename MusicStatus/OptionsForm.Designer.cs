195f197b67f9b22c79b62070b8cdc9f24a3ce13f
sunbiz@gmail.com <sunbiz@gmail.com@ad07b227-645d-dc72-f178-11e69cb75f21>
2011-01-05 19:48:49 +0000

    initial commit with basic code

---
+++ MusicStatus/OptionsForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MusicStatus
+{
+    partial class OptionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsForm));
+            this.cmdStatus = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmdStatus
+            // 
+            this.cmdStatus.Location = new System.Drawing.Point(405, 115);
+            this.cmdStatus.Name = "cmdStatus";
+            this.cmdStatus.Size = new System.Drawing.Size(75, 23);
+            this.cmdStatus.TabIndex = 0;
+            this.cmdStatus.Text = "Set Status";
+            this.cmdStatus.UseVisualStyleBackColor = true;
+            this.cmdStatus.Click += new System.EventHandler(this.cmdStatus_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(12, 115);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(386, 20);
+            this.txtStatus.TabIndex = 2;
+            // 
+            // OptionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 147);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.cmdStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OptionsForm";
+            this.Text = "Music Status - Let the world know your music";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdStatus;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
